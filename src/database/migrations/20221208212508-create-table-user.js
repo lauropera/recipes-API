083ebc0182044ba2265f1b07ec40be655ff37f5d
Lauro Pereira <lauropera01@gmail.com>
2022-12-08 18:28:12 -0300

    fix: droptable for migration user

--- src/database/migrations/20221208212508-create-table-user.js
+++ src/database/migrations/20221208212508-create-table-user.js
@@ -27,11 +27,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('user');
   }
 };
