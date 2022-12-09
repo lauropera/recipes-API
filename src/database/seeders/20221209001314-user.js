6a2e7302aee9c1d0dab5f37dee35e13aeb8d42c6
Lauro Pereira <lauropera01@gmail.com>
2022-12-08 21:15:27 -0300

    feat: user seed

---
+++ src/database/seeders/20221209001314-user.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert('user', [
+      {
+        name: 'Mallu Magalhães',
+        email: 'mallu@artist.com',
+        password:
+          '$2a$08$1cg8tH8gaoXr77hMTkb/SOS/ixfvwRJV93dVdAk6Z/c5UMjmD2wyq',
+      },
+      {
+        name: 'Sebastian',
+        email: 'sebastian@sebs.com',
+        password:
+          '$2a$08$mjEvVqV72LHMKTlTZXlBS.C74b8Uq6uRvPgGu5k5.xQ6koVzeontO',
+      },
+    ]);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('user', null, {});
+  },
+};
