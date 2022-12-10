b813300d1b81335dd3362dadbba2c2914f8d1c22
Lauro Pereira <lauropera01@gmail.com>
2022-12-10 01:19:20 -0300

    feat: new recipe schema

--- src/services/utils/validations/schemas.ts
+++ src/services/utils/validations/schemas.ts
@@ -1,16 +1,48 @@
 import { z } from 'zod';
 
 const REQUIRED_MSG = 'Some required fields are missing';
+const EMAIL_MSG = 'Invalid email';
+
+const setLengthMessage = (field: string, lengthNumber: number) =>
+  `${field} must have at least ${lengthNumber} characters`;
 
 export const LoginSchema = z.object({
-  email: z.string({ required_error: REQUIRED_MSG }).email('Invalid email'),
+  email: z.string({ required_error: REQUIRED_MSG }).email(EMAIL_MSG),
   password: z
     .string({ required_error: REQUIRED_MSG })
-    .min(4, 'Password must be at least 4 characters'),
+    .min(4, setLengthMessage('Password', 4)),
 });
 
 export const RegisterSchema = LoginSchema.extend({
   name: z
     .string({ required_error: REQUIRED_MSG })
-    .min(2, 'Name must be at least 2 characters'),
+    .min(2, setLengthMessage('Name', 2)),
+});
+
+export const RecipeSchema = z.object({
+  name: z
+    .string({ required_error: REQUIRED_MSG })
+    .min(2, setLengthMessage('Recipe name', 2)),
+  chef: z.string({ required_error: REQUIRED_MSG }).email(EMAIL_MSG),
+  preparationTime: z
+    .number({ required_error: REQUIRED_MSG })
+    .positive('Invalid preparation time'),
+  servings: z
+    .number({ required_error: REQUIRED_MSG })
+    .positive('Invalid serving quantity'),
+  videoUrl: z.string().optional(),
+  imageUrl: z.string().optional(),
+  category: z.string({ required_error: REQUIRED_MSG }),
+  tags: z.array(z.string()).optional(),
+  ingredients: z
+    .array(
+      z.object({
+        amount: z.number().positive('Invalid amount'),
+        name: z.string().min(2, setLengthMessage('Ingredient name', 2)),
+        unit: z.string().optional(),
+      }),
+      { required_error: REQUIRED_MSG }
+    )
+    .nonempty(),
+  instructions: z.array(z.string()).nonempty(),
 });
