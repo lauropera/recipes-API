4bd00ba8f7b625b107b434fd85ee08a6d4548c28
Lauro Pereira <lauropera01@gmail.com>
2022-12-10 17:34:34 -0300

    remove categoryService from instances

--- src/routes/controllerInstances.ts
+++ src/routes/controllerInstances.ts
@@ -1,13 +1,11 @@
 import {
   AuthController,
   RecipeController,
-  CategoryController,
 } from '../controllers';
 
 const authController = new AuthController();
 
 const recipeController = new RecipeController();
 
-const categoryController = new CategoryController();
 
-export { authController, recipeController, categoryController };
+export { authController, recipeController };
