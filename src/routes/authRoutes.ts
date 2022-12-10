dd2c2f2e59457c5aa859a262915bea8b752d0d0b
Lauro Pereira <lauropera01@gmail.com>
2022-12-10 16:53:25 -0300

    removing validate route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,10 @@
 import 'express-async-errors';
 import { Router } from 'express';
 import { authController } from './controllerInstances';
-import authMiddleware from '../middlewares/authMiddleware';
 
 const router = Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.get('/validate', authMiddleware, authController.validate);
 
 export default router;
